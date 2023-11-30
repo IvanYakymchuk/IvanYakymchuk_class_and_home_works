7d65e0422c58cfd1262df648f870c36272467566
IvanYakymchuk <146486156+IvanYakymchuk@users.noreply.github.com>
2023-11-30 21:55:55 +0200

    Update class_work_5.py

--- class_work_5.py
+++ class_work_5.py
@@ -58,6 +58,65 @@ class Painting(Work_of_art, Technique):
     def change_price(self):
         self.price = int(input())
 
+class Sorted:
+    def __init__(self, obj) -> None:
+        self.obj = obj
+        self.obj = self.__iter__()
+
+    def __iter__(self):
+        return iter(sorted(self.obj))
+
+class SortedSet(Sorted, set):
+    def __init__(self, obj):
+        Sorted.__init__(self, obj)
+
+    def __str__(self):
+        return set(self.obj).__str__()
+
+class Point:
+    def __init__(self) -> None:
+        self.x = 0
+        self.y = 0
+
+    def __str__(self) -> str:
+        return f'({self.x}, {self.y})'
+
+    def input(self):
+        self.x = float(input('x = '))
+        self.y = float(input('y = '))
+
+class CompareMixin:
+    def __eq__(self, other):
+        return not (self.__lt__(other) or other.__lt__(self))
+
+    def __gt__(self, other):
+        return other.__lt__(self)
+
+    def __ne__(self, other):
+        return self.__lt__(other) or other.__lt__(self)
+
+    def __ge__(self, other):
+        return not self.__lt__(other)
+
+    def __le__(self, other):
+        return not other.__lt__(self)
+
+
+class Point2(Point, CompareMixin):
+    pass
+
+
+class XOrderPoint2(Point2):
+    def __lt__(self, other):
+        return self.x < other.x
+
+class YOrderPoint2(Point2):
+    def __lt__(self, other):
+        return self.y < other.y
+
+class DistOrderPoint2(Point2):
+    def __lt__(self, other):
+        return (self.x**2 + self.y**2)**0.5 < (other.x**2 + other.y**2)**0.5
 
 if __name__ == "__main__":
 
@@ -94,6 +153,22 @@ if __name__ == "__main__":
             continue
 
         print(f"{ind_max} придбав {painting} за {maxp}")
+
+#до 18_8
+    s = SortedSet([3,6,-8,1,9,-4,2])
+    print(s)
+#до 18_10
+
+     n = int(input('n = '))
+    points_list = []
+    for _ in range(n):
+        a = DistOrderPoint2()
+        a.input()
+        points_list.append(a)
+    points_list = sorted(points_list)[::-1]
+    for i in points_list:
+        print(i)
+
 
 
 
