55cbf19a885e12b5c890a4aacb49f6f646a5567f
IvanYakymchuk <146486156+IvanYakymchuk@users.noreply.github.com>
2023-12-03 14:50:07 +0200

    Update home_work_4 .py

--- home_work_4 .py
+++ home_work_4 .py
@@ -13,11 +13,8 @@ def remove_duplicates_and_sort(func):
 def get_words_from_file(file_path):
     words = []
     try:
-        # Відкриваємо текстовий файл за допомогою конструкції with
         with open(file_path, 'r', encoding='utf-8') as file:
-            # Зчитуємо кожен рядок файлу
             for line in file:
-                # Використовуємо регулярний вираз для виділення лише буквених слів
                 words.extend(re.findall(r'\b\w+\b', line.lower()))
     except FileNotFoundError:
         print(f"Файл {file_path} не знайдено.")
